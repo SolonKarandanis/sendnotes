a12bcce568f245dcbe2ac3f1ade58a8d68ff3858
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-11-09 11:16:59 +0200

    Changes in models

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -9,4 +9,17 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     use HasFactory, HasUuids;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    protected $casts = [
+        'is_published' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
