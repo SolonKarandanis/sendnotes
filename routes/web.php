4ff2d26e3766a67d2e881b144979cbb2533d3e27
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-11-09 12:30:09 +0200

    Created notes page

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,8 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
+Route::view('notes', 'notes.notes')
+    ->middleware(['auth'])
+    ->name('notes.notes');
+
 require __DIR__.'/auth.php';
