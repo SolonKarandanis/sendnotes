fcec0d6e6712a8170750206e097a3f4f18e49bee
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-11-10 12:30:44 +0200

    Created trait

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
-    use HasFactory, HasUuids;
+    use HasFactory, Uuid;
 
     protected $guarded = [
         'id',
