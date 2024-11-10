cc125246f64f1d9b1e27f695aeb56c83c749dba5
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-11-10 11:30:19 +0200

    Created create-note component

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -8,7 +8,8 @@
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="p-6 text-gray-900 dark:text-gray-100">
-                <livewire:notes.show-notes />
+                <x-button icon="arrow-left" class="mb-8" href="{{ route('notes.notes') }}">All Notes</x-button>
+                <livewire:notes.create-note />
             </div>
         </div>
     </div>
