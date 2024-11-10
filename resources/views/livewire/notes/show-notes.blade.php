9efe3af27b76c24513db44a36680a3f88f4ae4bb
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-11-10 11:14:22 +0200

    Added recipient to notes table

--- resources/views/livewire/notes/show-notes.blade.php
+++ resources/views/livewire/notes/show-notes.blade.php
@@ -13,17 +13,28 @@ new class extends Component {
 
 <div>
     <div class="space-y-2">
-        @foreach ($notes as $note)
-            <x-card wire:key='{{$note->id}}'>
-                <div class="flex justify-between">
-                    <a href="#" class="text-xl font-bold hover:underline hover:text-blue-500">
-                        {{$note->title }}
-                    </a>
-                    <div class="text-xs text-gray-500">
-                        {{ \Carbon\Carbon::parse($note->send_date)->format('M-d-Y') }}
+        <div class="grid gap-4 mt-12 grid-col-2">
+            @foreach ($notes as $note)
+                <x-card wire:key='{{$note->id}}'>
+                    <div class="flex justify-between">
+                        <a href="#" class="text-xl font-bold hover:underline hover:text-blue-500">
+                            {{$note->title }}
+                        </a>
+                        <div class="text-xs text-gray-500">
+                            {{ \Carbon\Carbon::parse($note->send_date)->format('M-d-Y') }}
+                        </div>
                     </div>
-                </div>
-            </x-card>
-        @endforeach
+                    <div class="flex items-end justify-between mt-4 space-x-1">
+                        <p class="text-xs">
+                            Recipient: <span class="font-semibold"></span>
+                        </p>
+                        <div>
+                            <x-button.circle icon="eye"></x-button>
+                            <x-button.circle icon="trash"></x-button>
+                        </div>
+                    </div>
+                </x-card>
+            @endforeach
+        </div>
     </div>
 </div>
